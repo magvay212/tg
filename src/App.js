e31e28220847553626bea1a6e78e9031f203b6eb
Данила Верхоланцев <danilaverholancev@Danilas-MacBook-Air.local>
2024-10-13 11:21:23 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { useEffect } from 'react';
 import './App.css';
+import { useTelegram } from './hooks/useTelegram';
 
 function App() {
+  const {onToggleButton, tg} = useTelegram();
+
+
+
   useEffect(() => {
     tg.ready();
   }, [])
 
   return (
     <div className="App">
-    work
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
