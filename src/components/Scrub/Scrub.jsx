4f0b0729030f6ece02bfd0c620ea3d8c7abef7fe
Данила Верхоланцев <danilaverholancev@Danilas-MacBook-Air.local>
2024-10-14 20:33:42 +0300

    change button

--- src/components/Scrub/Scrub.jsx
+++ src/components/Scrub/Scrub.jsx
@@ -21,29 +21,6 @@ const ProductList = () => {
     const [addedItems, setAddedItems] = useState([])
     const {tg} = useTelegram();
 
-    const onSendData = useCallback(() => {
-        const data = {
-            products: addedItems,
-            totalPrice: getTotalPrice(addedItems),
-        }
-        fetch('http://localhost:8000', {
-            method: 'POST', 
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-
-    }, [])
-
-    useEffect(() => {
-        tg.onEvent('mainButtonClicked', onSendData)
-        return () => {
-            tg.offEvent('mainButtonClicked', onSendData)
-        }
-
-    }, [onSendData])
-
 
     const onAdd = (product) => {
         const alreadyAdded = addedItems.find(item => item.id === product.id);
