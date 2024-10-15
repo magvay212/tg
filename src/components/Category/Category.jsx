ac1a5ce9b1cc0ec4b682c4df0d11b6978e90ea66
magvay212 <72155661+magvay212@users.noreply.github.com>
2024-10-15 13:38:02 +0300

    fix

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -4,14 +4,14 @@ import CategoryItem from "../CategoryItem/CategoryItem";
 
 
 const catlist = [
-    {id: '1', title: 'Скрабы', image: 'src/components/Category/img/scrub.jpg'},
-    {id: '2', title: 'Уходовая косметика', image: 'src/components/Category/img/skincare.jpg'},
-    {id: '3', title: 'Ароматерапия', image: 'src/components/Category/img/aromatherapy.jpg'},
-    {id: '4', title: 'Свечи', image: 'src/components/Category/img/candles.jpg'},
-    {id: '5', title: 'Мыло', image: 'src/components/Category/img/soap.jpg'},
-    {id: '6', title: 'Для волос', image: 'src/components/Category/img/hair.jpg'},
-    {id: '7', title: 'Для дома', image: 'src/components/Category/img/home.jpg'},
-    {id: '8', title: 'Сезонное предложение', image: 'src/components/Category/img/seasonal.jpg'},
+    {id: '1', title: 'Скрабы', image: '/src/components/Category/image/scrub.jpg'},
+    {id: '2', title: 'Уходовая косметика', image: '/src/components/Category/image/skincare.jpg'},
+    {id: '3', title: 'Ароматерапия', image: '/src/components/Category/image/aromatherapy.jpg'},
+    {id: '4', title: 'Свечи', image: '/src/components/Category/image/candles.jpg'},
+    {id: '5', title: 'Мыло', image: '/src/components/Category/image/soap.jpg'},
+    {id: '6', title: 'Для волос', image: '/src/components/Category/image/hair.jpg'},
+    {id: '7', title: 'Для дома', image: '/src/components/Category/image/home.jpg'},
+    {id: '8', title: 'Сезонное предложение', image: '/src/components/Category/image/seasonal.jpg'},
 ];
 
 
