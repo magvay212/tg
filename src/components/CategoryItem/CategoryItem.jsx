cbc6e62d00e845f1d21414d47f5b81c1dce2dde7
magvay212 <72155661+magvay212@users.noreply.github.com>
2024-10-15 12:53:20 +0300

    add img

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const CategoryItem = ({category, className}) => {
 
     return (
-        <div className={'category ' + className}>
-            <div className={'img'}>{category.Image}</div>
-            <div className={'title'}>{category.title}</div>
+        <div className='category-item'>
+            <img src={category.image} alt={category.title} className='category-image' />
+            <h3 className='category-title'>{category.title}</h3>
         </div>
+        //<div className={'category ' + className}>
+        //    <div className={'img'}>{category.Image}</div>
+        //    <div className={'title'}>{category.title}</div>
+        //</div>
 
     );
 
